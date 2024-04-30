f077f140ec3fba4a72b76f51e44c1bb63ae0d346
almeng <almeng1234556@gmail.com>
2024-04-30 00:50:22 -0700

    ast/node: add comment

--- pkg/jindo/ast/node.go
+++ pkg/jindo/ast/node.go
@@ -1,9 +1,57 @@
+// Copyright 2024 The Jindo Authors. All rights reserved.
+// Use of this source code is governed by a GPL-3 style
+// license that can be found in the LICENSE file.
+
+// Package ast declares the types used to represent syntax trees for Jindo
+// packages.
+
 package ast
 
 import (
 	"jindo/pkg/jindo/scanner"
 	"jindo/pkg/jindo/token"
 )
+
+// ----------------------------------------------------------------------------
+// Interfaces
+//
+// There are 3 main classes of nodes: Expressions and type nodes,
+// statement nodes, and declaration nodes. The node names usually
+// match the corresponding Jindo spec production names to which they
+// correspond. The node fields correspond to the individual parts
+// of the respective productions.
+//
+// All nodes contain position information marking the beginning of
+// the corresponding source text segment; it is accessible via the
+// Pos accessor method. Nodes may contain additional position info
+// for language constructs where comments may be found between parts
+// of the construct (typically any larger, parenthesized subpart).
+// That position information is needed to properly position comments
+// when printing the construct.
+
+//// All node types implement the Node interface.
+//type Node interface {
+//	Pos() token.Pos // position of first character belonging to the node
+//	End() token.Pos // position of first character immediately after the node
+//}
+//
+//// All expression nodes implement the Expr interface.
+//type Expr interface {
+//	Node
+//	exprNode()
+//}
+//
+//// All statement nodes implement the Stmt interface.
+//type Stmt interface {
+//	Node
+//	stmtNode()
+//}
+//
+//// All declaration nodes implement the Decl interface.
+//type Decl interface {
+//	Node
+//	declNode()
+//}
 
 type Node interface {
 	GetPos() scanner.Pos
