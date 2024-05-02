69bc28664101b6062e7a7c0f3916eb289e9ab2de
almeng <almeng1234556@gmail.com>
2024-05-01 18:29:58 -0700

    Setup initial command.go for command definition

---
+++ cmd/jindo/command/command.go
@@ -0,0 +1,10 @@
+// Copyright 2024 The Jindo Authors. All rights reserved.
+// This file is part of jindo and is licensed under
+// the GNU General Public License version 3, which is available at
+// https://www.gnu.org/licenses/gpl-3.0.html or in the LICENSE file
+// located in the root directory of this source tree.
+
+package command
+
+type Command struct {
+}
