3f9cad6e02e390e5f8457ae03e8296ae1b5cdfe8
almeng <almeng1234556@gmail.com>
2024-05-01 22:29:16 -0700

    init internal/types

---
+++ pkg/jindo/internal/types/type.go
@@ -0,0 +1,17 @@
+// Copyright 2024 The Jindo Authors. All rights reserved.
+// This file is part of jindo and is licensed under
+// the GNU General Public License version 3, which is available at
+// https://www.gnu.org/licenses/gpl-3.0.html or in the LICENSE file
+// located in the root directory of this source tree.
+
+package types
+
+// A Type represents a type of Jindo.
+// All types implement the Type interface.
+type Type interface {
+	// Underlying returns the underlying type of a type.
+	Underlying() Type
+
+	// String returns a string representation of a type.
+	String() string
+}
