210b4e0d0813f3fffb2f8070bf5dc5570d7c31fb
almeng <almeng1234556@gmail.com>
2024-11-22 15:50:42 +0900

    rename pkg.go to space.go

--- cmd/jindo/compile/types/space.go
+++ cmd/jindo/compile/types/space.go
@@ -47,12 +47,12 @@ func PathToPrefix(s string) string {
 	return string(p)
 }
 
-// pkgMap maps a package path to a package.
-var pkgMap = make(map[string]*Pkg)
+// spaceMap maps a space path to a space.
+var spaceMap = make(map[string]*Space)
 
-type Pkg struct {
+type Space struct {
 	Path   string // string literal used in import statement, e.g. "internal/runtime/sys"
-	Name   string // package name, e.g. "sys"
+	Name   string // space name, e.g. "sys"
 	Prefix string // escaped path for use in symbol table
 	Syms   map[string]*Sym
 	// Pathsym *obj.LSym
@@ -60,23 +60,23 @@ type Pkg struct {
 	Direct bool // imported directly
 }
 
-// NewPkg returns a new Pkg for the given package path and name.
-// Unless name is the empty string, if the package exists already,
-// the existing package name and the provided name must match.
-func NewPkg(path, name string) *Pkg {
-	if p := pkgMap[path]; p != nil {
+// NewSpace returns a new space for the given space path and name.
+// Unless name is the empty string, if the space exists already,
+// the existing space name and the provided name must match.
+func NewSpace(path, name string) *Space {
+	if p := spaceMap[path]; p != nil {
 		if name != "" && p.Name != name {
-			panic(fmt.Sprintf("conflicting package names %s and %s for path %q", p.Name, name, path))
+			panic(fmt.Sprintf("conflicting space names %s and %s for path %q", p.Name, name, path))
 		}
 		return p
 	}
 
-	p := new(Pkg)
+	p := new(Space)
 	p.Path = path
 	p.Name = name
 	if path == "go.shape" {
 		// Don't escape "go.shape", since it's not needed (it's a builtin
-		// package), and we don't want escape codes showing up in shape type
+		// space), and we don't want escape codes showing up in shape type
 		// names, which also appear in names of function/method
 		// instantiations.
 		p.Prefix = path
@@ -84,69 +84,69 @@ func NewPkg(path, name string) *Pkg {
 		p.Prefix = PathToPrefix(path)
 	}
 	p.Syms = make(map[string]*Sym)
-	pkgMap[path] = p
+	spaceMap[path] = p
 
 	return p
 }
 
-func PkgMap() map[string]*Pkg {
-	return pkgMap
+func SpaceMap() map[string]*Space {
+	return spaceMap
 }
 
-var nopkg = &Pkg{
+var nospace = &Space{
 	Syms: make(map[string]*Sym),
 }
 
-func (pkg *Pkg) Lookup(name string) *Sym {
-	s, _ := pkg.LookupOK(name)
+func (space *Space) Lookup(name string) *Sym {
+	s, _ := space.LookupOK(name)
 	return s
 }
 
-// LookupOK looks up name in pkg and reports whether it previously existed.
-func (pkg *Pkg) LookupOK(name string) (s *Sym, existed bool) {
+// LookupOK looks up name in space and reports whether it previously existed.
+func (space *Space) LookupOK(name string) (s *Sym, existed bool) {
 	// TODO(gri) remove this check in favor of specialized lookup
-	if pkg == nil {
-		pkg = nopkg
+	if space == nil {
+		space = nospace
 	}
-	if s := pkg.Syms[name]; s != nil {
+	if s := space.Syms[name]; s != nil {
 		return s, true
 	}
 
 	s = &Sym{
 		Name: name,
-		Pkg:  pkg,
+		Space:  space,
 	}
-	pkg.Syms[name] = s
+	space.Syms[name] = s
 	return s, false
 }
 
-func (pkg *Pkg) LookupBytes(name []byte) *Sym {
+func (space *Space) LookupBytes(name []byte) *Sym {
 	// TODO(gri) remove this check in favor of specialized lookup
-	if pkg == nil {
-		pkg = nopkg
+	if space == nil {
+		space = nospace
 	}
-	if s := pkg.Syms[string(name)]; s != nil {
+	if s := space.Syms[string(name)]; s != nil {
 		return s
 	}
 	str := InternString(name)
-	return pkg.Lookup(str)
+	return space.Lookup(str)
 }
 
 // LookupNum looks up the symbol starting with prefix and ending with
 // the decimal n. If prefix is too long, LookupNum panics.
-func (pkg *Pkg) LookupNum(prefix string, n int) *Sym {
+func (space *Space) LookupNum(prefix string, n int) *Sym {
 	var buf [20]byte // plenty long enough for all current users
 	copy(buf[:], prefix)
 	b := strconv.AppendInt(buf[:len(prefix)], int64(n), 10)
-	return pkg.LookupBytes(b)
+	return space.LookupBytes(b)
 }
 
 // Selector looks up a selector identifier.
-func (pkg *Pkg) Selector(name string) *Sym {
+func (space *Space) Selector(name string) *Sym {
 	if IsExported(name) {
-		pkg = LocalPkg
+		space = LocalSpace
 	}
-	return pkg.Lookup(name)
+	return space.Lookup(name)
 }
 
 var (
